e545862220cbf72b03de1f319f44d80af52cc36c
PLeVeL1857 <smagulovabilai7@gmail.com>
2023-04-05 02:40:18 +0600

    best

--- assets/js/ro.js
+++ assets/js/ro.js
@@ -15,7 +15,7 @@ var colorsExamples = anime.timeline({
     { value: '#DAA520', duration: 3000 },
     { value: '#FF0000', duration: 3000 },
     { value: '#808080', duration: 3000 },
-    { value: '#20202c', duration: 1500 },       
+    { value: '#17171a', duration: 1500 },       
 // добавляем первое значение в конец массива
   ],
   duration: 1000,
